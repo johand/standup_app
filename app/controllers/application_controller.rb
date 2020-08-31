1f40648746c675a2de1f174cf98edff49152a4f6
Johan <johand@openmailbox.org>
2020-08-31 00:16:16 -0400

    Devise sign up and error helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
 end
