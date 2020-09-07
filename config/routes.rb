211e432c8216ac8257da5a8aaac2eb7e0d947bbc
Johan <johand@openmailbox.org>
2020-09-07 00:55:49 -0400

    Accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :accounts
   get 'activity/mine'
   get 'activity/feed'
   root to: 'activity#mine'
