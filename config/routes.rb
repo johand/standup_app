0e14222e8de731ba39b911e3d660669fda50b4a8
Johan <johand@openmailbox.org>
2020-09-13 23:44:42 -0400

    Managing the users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :accounts
+
+  get 'user/me', to: 'users#me', as: 'my_settings'
+  patch 'user/update_me', to: 'users#update_me', as: 'update_my_settings'
+  get 'user/password', to: 'users#password', as: 'my_password'
+  patch 'user/update_password', to: 'users#update_password', as: 'my_update_password'
+
+  scope 'account', as: 'account' do
+    resources :users
+  end
+
   get 'activity/mine'
   get 'activity/feed'
   root to: 'activity#mine'
