8225972cc6facea372fae0bb2f126b47473f39f5
Johan <johand@openmailbox.org>
2022-04-16 19:41:55 -0400

    Fixing SendEventJob job
    Avoiding error when it doesn't find a user.id

--- app/jobs/analytics/mixpanel/send_event_job.rb
+++ app/jobs/analytics/mixpanel/send_event_job.rb
@@ -4,7 +4,7 @@ module Analytics
   module Mixpanel
     class SendEventJob < ApplicationJob
       def perform(user, event_name, event_data)
-        TRACKER.track(user.id, event_name, event_data)
+        TRACKER.track(user.id, event_name, event_data) if user
       end
     end
   end
